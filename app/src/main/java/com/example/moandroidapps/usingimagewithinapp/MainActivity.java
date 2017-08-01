80b96b2580d5d9f2b149d2efc3dad03485e69a79
Muhammad Owais Aslam <rana.owais.aslam@gmail.com>
2017-08-02 01:32:51 +0500

    Now, an image at the end is also added. Which will be uploaded on Runtime.

--- app/src/main/java/com/example/moandroidapps/usingimagewithinapp/MainActivity.java
+++ app/src/main/java/com/example/moandroidapps/usingimagewithinapp/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.moandroidapps.usingimagewithinapp;
 
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,11 +29,23 @@ public class MainActivity extends AppCompatActivity {
 
 
         //Adding Image on Runtime:
+        //Here we are working with Resources
+        //Recommendation: When you are working with single image. Keep everything in resources folder.
         ImageView iv = (ImageView) findViewById(R.id.another_image_view);
-        iv.setImageResource(R.drawable.logo1);
+        //iv.setImageResource(R.drawable.logo1);
 
-
-
-
-    }
+        //Finding Image file in the assets folder:
+        //Here we are working with Assets
+        //Recommendation: When you are working with multiple images. Keep them in assets folder.
+        //It will make your app run faster. Because, assets folder is not compiled into an application.
+        // And, Resource identifiers are not assigned.
+        String fileName = "logo1.png";
+        try {
+            InputStream inputStream = getAssets().open(fileName);
+            Drawable drawable = Drawable.createFromStream(inputStream, null);
+            iv.setImageDrawable(drawable);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }//end catch
+    }//onCreate
 }
